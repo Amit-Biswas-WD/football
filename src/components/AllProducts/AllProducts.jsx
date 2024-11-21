6c3f88550d533305e7ad0f94d7f609038e6f3764
Amit Biswas <amitbiswas9282@gmail.com>
2024-11-21 18:15:44 +0600

    Last

--- src/components/AllProducts/AllProducts.jsx
+++ src/components/AllProducts/AllProducts.jsx
@@ -14,7 +14,6 @@ const AllProducts = ({ handleSelectedProduct }) => {
 
   return (
     <div className="col-span-4">
-      <h2>This is All Products Page: {products.length}</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product) => (
           <SingleProduct
